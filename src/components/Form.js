aff7903d2c18c5b4f9a32f49a9f18f293577aa98
Anya <anya.p.nguyen@gmail.com>
2019-04-17 17:06:36 -0700

    completed styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,78 +1,87 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Form.css";
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      where: "",
+      when: "",
+      errors: {}
+    };
+  }
 
-	constructor(props){
-		super(props);
-		this.state = {
-			name: '',
-			where: '',
-			when: '',
-			errors: {}
-		}
-	}
-
-	handleSubmit(e){
-		e.preventDefault();
-		this.props.scheduleEvent({
-			name: this.state.name,
-			where: this.state.where,
-			when: this.state.when,
-			weekday: this.props.weekday,
-			hour: this.props.hour,
-			timeSlot: this.props.timeSlot,
-			timeSlots: this.props.selectedTimeSlots,
-		})
-	}
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.scheduleEvent({
+      name: this.state.name,
+      where: this.state.where,
+      when: this.state.when,
+      weekday: this.props.weekday,
+      hour: this.props.hour,
+      timeSlot: this.props.timeSlot,
+      timeSlots: this.props.selectedTimeSlots
+    });
+  }
 
   render() {
     return (
-				<div className="Form">
+      <div className="Form">
         <form onSubmit={this.handleSubmit.bind(this)}>
-						<h3>Don't see a time that suits you? Create your event:</h3>
-            <label>Name</label>
-						<input 
-							type="text" 
-							name="name" 
-							value={this.state.name}
-							onChange={(e) => {this.setState({name: e.target.value})}}
-						/>
-						<br />
-            <label>Where: </label>
-						<input 
-							type="text" 
-							name="where" 
-							value={this.state.where}
-							onChange={(e) => {this.setState({where: e.target.value})}}
-						/>
-						<br />
-            <label>When: </label>
-						<input 
-							type="text" 
-							name="when" 
-							value={this.state.when}
-							onChange={(e) => {this.setState({when: e.target.value})}}
-						/>
-						<br />
-						<input className="button btn btn-default"
-							type="submit" 
-							value="Schedule"
-							disabled={!this.state.name || !this.state.where || !this.state.when}
-						/>
-
+          <label>Name </label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={e => {
+              this.setState({ name: e.target.value });
+            }}
+          />
+          <br />
+          <label>Where </label>
+          <input
+            type="text"
+            name="where"
+            value={this.state.where}
+            onChange={e => {
+              this.setState({ where: e.target.value });
+            }}
+          />
+          <br />
+          <label>When </label>
+          <input
+            type="text"
+            name="when"
+            value={this.state.when}
+            onChange={e => {
+              this.setState({ when: e.target.value });
+            }}
+          />
+          <br />
+          <div className="schedule">
+            <input
+              className="button btn btn-default "
+              type="submit"
+              value="Schedule"
+              disabled={
+                !this.state.name || !this.state.where || !this.state.when
+              }
+            />
+          </div>
         </form>
       </div>
     );
   }
 }
 
-const mapStoreToProps = (store) => ({
-	//need to change to choose multiple cells at server end
-	selectedTimeSlots: store.main.selectedTimeSlots,
-	weekday: store.main.selectedTimeSlots[0].weekday,
-	hour: store.main.selectedTimeSlots[0].hour,
-	timeSlot: store.main.selectedTimeSlots[0].timeSlot,
-})
+const mapStoreToProps = store => ({
+  //need to change to choose multiple cells at server end
+  selectedTimeSlots: store.main.selectedTimeSlots,
+  weekday: store.main.selectedTimeSlots[0].weekday,
+  hour: store.main.selectedTimeSlots[0].hour,
+  timeSlot: store.main.selectedTimeSlots[0].timeSlot
+});
 
 export default connect(mapStoreToProps)(Form);
--- src/components/ScheduleCell.js
+++ src/components/ScheduleCell.js
@@ -1,81 +1,84 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import "./ScheduleCell.css";
 
-function ScheduleCell({joiners, currentUsername, owner, name, purpose, where, deleteEvents, requestEvent, cancelEvent, onAddSession, ...otherProps }) {
-    console.log('here', joiners, currentUsername)
-        const areYouOwner = owner.username === currentUsername;
-        const areYouAJoiner = joiners && joiners.map(u => u.username).includes(currentUsername)
+function ScheduleCell({
+  joiners,
+  currentUsername,
+  owner,
+  name,
+  purpose,
+  where,
+  deleteEvents,
+  requestEvent,
+  cancelEvent,
+  onAddSession,
+  ...otherProps
+}) {
+  console.log("here", joiners, currentUsername);
+  const areYouOwner = owner.username === currentUsername;
+  const areYouAJoiner =
+    joiners && joiners.map(u => u.username).includes(currentUsername);
 
-
-        return (
-            <div className="cell for-phone-only for-tablet-portrait-up for-tablet-portait-only">
-                <h3>{name}</h3>
-                <p>{purpose}</p>
-                <p>{where}</p>
-                {areYouOwner
-                &&
-                joiners.length > 0
-                &&
-                <p>
-                    <strong>
-                        Scheduled with {joiners[0].username}
-                    </strong>
-                </p>
-
-                }
-                {areYouOwner
-                 &&
-                <button  className="button btn btn-default for-phone-only for-tablet-portrait-up for-tablet-portait-only"
-                    onClick={
-                        e => {e.stopPropagation() // to stop bubbling up and prevent the form to show up
-                             deleteEvents()
-                        }
-                        
-                    } 
-                   
-                >
-                    Delete
-                </button>
-                }
-                {!areYouOwner && !areYouAJoiner
-                &&
-                <div>
-                <button className="button btn btn-default for-phone-only for-tablet-portrait-up for-tablet-portait-only"
-                    onClick={
-                        e => {e.stopPropagation()
-                        requestEvent()
-                        }
-                    }
-                >
-                Request
-                </button>
-               
-                </div>
-                }
-                {!areYouOwner && areYouAJoiner && 
-                            <div>
-                            <p><strong> Scheduled! </strong></p>
-                            <button className="button btn btn-default for-phone-only for-tablet-portrait-up for-tablet-portait-only"
-                                onClick={
-                                    e => {e.stopPropagation()
-                                    cancelEvent()
-                                    }
-                                }
-                            >
-                            Cancel
-                            </button>
-                           
-                            </div>    
-            
-
-                }
-            </div>
-        )
+  return (
+    <div className="cell">
+      <div className="info-p1">
+        <p>{name}</p>
+      </div>
+      <div className="info-p2">
+        <p>{where}</p>
+      </div>
+      {areYouOwner && joiners.length > 0 && (
+        <p>
+          <strong>Scheduled with {joiners[0].username}</strong>
+        </p>
+      )}
+      {areYouOwner && (
+        <button
+          className="button btn btn-default for-phone-only for-tablet-portrait-up for-tablet-portait-only"
+          onClick={e => {
+            e.stopPropagation(); // to stop bubbling up and prevent the form to show up
+            deleteEvents();
+          }}
+        >
+          Delete
+        </button>
+      )}
+      {!areYouOwner && !areYouAJoiner && (
+        <div>
+          <button
+            className="button btn btn-default for-phone-only for-tablet-portrait-up for-tablet-portait-only"
+            onClick={e => {
+              e.stopPropagation();
+              requestEvent();
+            }}
+          >
+            Request
+          </button>
+        </div>
+      )}
+      {!areYouOwner && areYouAJoiner && (
+        <div className="buttonWraper">
+          <p className="Scheduled">
+            <strong> Scheduled! </strong>
+          </p>
+          <button
+            className="button btn btn-default for-phone-only for-tablet-portrait-up for-tablet-portait-only cancel"
+            onClick={e => {
+              e.stopPropagation();
+              cancelEvent();
+            }}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-    currentUsername: state.auth.currentUser.username
-
+const mapStateToProps = state => ({
+  currentUsername: state.auth.currentUser.username
 });
 
-export default connect(mapStateToProps)(ScheduleCell)
+export default connect(mapStateToProps)(ScheduleCell);
